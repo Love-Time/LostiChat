f85bc4e6083bee27d67639c45a7f18759feec002
Daniil <fyfik105@gmail.com>
2023-02-14 20:42:59 +0300

    fix

--- friends/serializers.py
+++ friends/serializers.py
@@ -6,11 +6,10 @@ from .models import Friends
 
 
 class FriendSerializer(serializers.ModelSerializer):
-    first_user = serializers.HiddenField(write_only=True, default=CurrentUserDefault())
     friend = UserSimpleSerializer(source='second_user')
     class Meta:
         model = Friends
-        exclude = ['second_user', 'first_user', 'accepted']
+        exclude = ['second_user', 'accepted']
 
 class FriendCreateSerializer(serializers.ModelSerializer):
     first_user = serializers.HiddenField(write_only=True, default=CurrentUserDefault())
