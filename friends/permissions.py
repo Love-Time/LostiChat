77a5983eb8aff3de9a649ec09e830039db14883e
Daniil <fyfik105@gmail.com>
2023-02-16 08:18:05 +0300

    optimized views

--- friends/permissions.py
+++ friends/permissions.py
@@ -1,14 +1,14 @@
 from rest_framework import permissions
 
 
-class IsOwner(permissions.BasePermission):
+class IsOwner(permissions.IsAuthenticated, permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if obj.first_user == request.user:
             return True
         return False
 
 
-class IsFriend(permissions.BasePermission):
+class IsFriend(permissions.IsAuthenticated, permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if obj.second_user == request.user:
             return True
--- friends/views.py
+++ friends/views.py
@@ -23,16 +23,19 @@ def contains(lst, filter):
 
 
 class FriendViewSet(mixins.CreateModelMixin,
-                   mixins.DestroyModelMixin,
-                   mixins.ListModelMixin,
-                   GenericViewSet):
+                    mixins.DestroyModelMixin,
+                    mixins.ListModelMixin,
+                    GenericViewSet):
     serializer_class = FriendSerializer
     queryset = Friends.objects.all()
     permission_classes = [IsAuthenticated]
+    lookup_field = 'second_user'
 
     def get_queryset(self):
         if self.action == "requests":
-            return Friends.objects.filter(Q(second_user_id=self.request.user.pk) & Q(accepted=False))
+            return Friends.objects.filter(Q(second_user_id=self.request.user.pk) & Q(accepted=0))
+        elif self.action == 'list':
+            return Friends.objects.filter(Q(first_user_id=self.request.user.id) & Q(accepted=1))
         return Friends.objects.filter(Q(first_user_id=self.request.user.pk) | Q(second_user_id=self.request.user.pk))
 
     def get_permissions(self):
@@ -42,16 +45,13 @@ class FriendViewSet(mixins.CreateModelMixin,
             return [IsFriend()]
         else:
             return [IsAuthenticated()]
+
     def get_serializer_class(self):
         if self.request.method == "POST":
             return FriendCreateSerializer
         elif self.action == 'requests':
             return FriendRequestSerializer
         return FriendSerializer
-
-
-    def list(self, request, *args, **kwargs):
-        return self.user(request, self.request.user.pk)
 
     @action(detail=False, methods=['get'])
     def requests(self, request):
@@ -60,29 +60,14 @@ class FriendViewSet(mixins.CreateModelMixin,
         return Response(serializer.data)
 
     @action(detail=True, methods=['patch'])
-    def denied(self, request, pk=None):
-        obj = get_object_or_404(Friends, Q(second_user=request.user) & Q(first_user_id=pk))
+    def denied(self, request, second_user=None):
+        obj = get_object_or_404(Friends, Q(first_user_id=second_user) & Q(second_user_id=request.user.pk))
         obj.accepted = -1
         obj.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     @action(detail=True, methods=['get'])
-    def user(self, request, pk=None):
-        if not isinstance(pk, int):
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        data = list(Friends.objects.filter(Q(first_user_id=pk) | Q(second_user_id=pk)))
-
-        me, another = [], []
-        for x in data:
-            (me, another)[x.first_user.pk != request.user.pk].append(x)
-
-        len_me = len(me) - 1
-        for i in range(len_me, -1, -1):
-
-            if not contains(another, lambda x: x.first_user.pk == me[i].second_user.pk and
-                                               x.second_user.pk == me[i].first_user.pk):
-                del me[i]
-
-        serializer = self.get_serializer(me, many=True)
+    def user(self, request, second_user=None):
+        data = Friends.objects.filter(Q(first_user_id=second_user) & Q(accepted=1))
+        serializer = self.get_serializer(data, many=True)
         return Response(serializer.data)
