e876399135c59199a77d292a963cb59b438f5e26
Daniil <fyfik105@gmail.com>
2023-03-11 17:35:56 +0300

    test

--- config/settings.py
+++ config/settings.py
@@ -138,12 +138,13 @@ try:
     }
     print(2)
 except KeyError:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
+    # }
+    print(3)
 
 
 
