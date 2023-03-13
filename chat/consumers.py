732b0f5b1ff7bafe6f888309d26a55138c33c11c
Daniil <fyfik105@gmail.com>
2023-03-13 22:16:22 +0300

    try

--- chat/consumers.py
+++ chat/consumers.py
@@ -57,10 +57,10 @@ class DialogMessageConsumer(mixins.CreateModelMixin,
         self.__start = True
         while self.queue:
             print(self.queue[0][2])
-            await self.create_dialog_message2(message=self.queue[0][2]['message'], recipient=self.queue[0][2]['recipient'], request_id=self.queue[0][2]['request_id'], action=self.queue[0][2]['action'])
-            # async_to_sync(channel_layer.group_send)(f'recipient_{data.sender.pk}',
-            #                                         {"type": "send_message", "data": data})
-            # print(data)
+            data = await self.create_dialog_message2(message=self.queue[0][2]['message'], recipient=self.queue[0][2]['recipient'], request_id=self.queue[0][2]['request_id'], action=self.queue[0][2]['action'])
+            async_to_sync(channel_layer.group_send)(f'recipient_{data.sender.pk}',
+                                                    {"type": "send_message", "data": data})
+
             break
         self.__start =  False
     @staticmethod
