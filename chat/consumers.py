a094d47ede3146e14a3abeef4e73eda361361de9
Daniil <fyfik105@gmail.com>
2023-03-13 21:50:35 +0300

    try

--- chat/consumers.py
+++ chat/consumers.py
@@ -57,7 +57,7 @@ class DialogMessageConsumer(mixins.CreateModelMixin,
         self.__start = True
         while self.queue:
             print(**self.queue[0][2])
-            data, status = self.queue[0][0](message=self.queue[0][2].message, recipient = )
+            data, status = self.queue[0][0](message=self.queue[0][2].message, recipient=self.queue[0][2].recipient)
             async_to_sync(channel_layer.group_send)(f'recipient_{data.sender.pk}',
                                                     {"type": "send_message", "data": data})
         self.__start =  False
