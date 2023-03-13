4aea19fdb4b02a58ec73f790363da23d014e5ff3
Daniil <fyfik105@gmail.com>
2023-03-13 22:43:03 +0300

    try

--- chat/consumers.py
+++ chat/consumers.py
@@ -55,17 +55,17 @@ class DialogMessageConsumer(mixins.CreateModelMixin,
         )
         await super().disconnect(code)
 
-    async def start_queue(self):
+    def start_queue(self):
         self.__start = True
         while self.queue:
             print(self.queue)
-            instance, data = await self.create_dialog_message2(message=self.queue[0][2]['message'], recipient=self.queue[0][2]['recipient'], request_id=self.queue[0][2]['request_id'], action=self.queue[0][2]['action'])
-            await channel_layer.group_send(f'recipient_{instance.sender.pk}',
+            instance, data = async_to_sync(self.create_dialog_message2)(message=self.queue[0][2]['message'], recipient=self.queue[0][2]['recipient'], request_id=self.queue[0][2]['request_id'], action=self.queue[0][2]['action'])
+            async_to_sync(channel_layer.group_send)(f'recipient_{instance.sender.pk}',
                                                     {"type": "send_message", "data": data})
-            # del self.queue[0]
-            # print("СПАТЬ")
-            # time.sleep(5)
-            break
+            del self.queue[0]
+            print("СПАТЬ")
+            time.sleep(5)
+
         self.__start = False
     @staticmethod
     def retry(func):
@@ -73,7 +73,7 @@ class DialogMessageConsumer(mixins.CreateModelMixin,
 
             self.queue.append((func, args, kwargs))
             if not self.__start:
-                print('startuem')
+                print('startuem', self)
                 await self.start_queue()
 
 
