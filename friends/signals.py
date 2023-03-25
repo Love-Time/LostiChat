6c8ff4ea713ca0baff6fda072e632907040025f2
Daniil <fyfik105@gmail.com>
2023-03-26 01:04:55 +0300

    'fix

--- friends/signals.py
+++ friends/signals.py
@@ -8,7 +8,15 @@ from users.serializers import UserSimpleSerializer
 from .models import Friends
 
 
-
+def send_friend(self, event):
+    async_to_sync(
+        self.send_json(
+            {
+                'data': event['data'],
+                'status': status.HTTP_200_OK,
+                'action': "friend"
+            }
+        ))
 channel_layer = get_channel_layer()
 
 
@@ -19,21 +27,21 @@ def create_user_channel(sender, instance: Friends, created, **kwargs):
     if not another_request and instance.accepted == 0:
         # Сказать второму, что ему отправили запрос в друзья
         async_to_sync(channel_layer.group_send)(f'recipient_{instance.second_user.id}',
-                                                {"type": "friend_message", "data": {'type': 'friend_request',
+                                                {"type": "send_friend", "data": {'type': 'friend_request',
                                                                                   'description': "Пришёл запрос в друзья",
                                                                                   'user': serializer.data}})
         return
     if instance.accepted == 1 and another_request == 1:
         # Сказать второму, что его запрос в друзья приняли
         async_to_sync(channel_layer.group_send)(f'recipient_{instance.second_user.id}',
-                                                {"type": "friend_message", "data": {'type': 'friend_accepted',
+                                                {"type": "send_friend", "data": {'type': 'friend_accepted',
                                                                                   'description': "Запрос в друзья принят",
                                                                                   'user': serializer.data}})
         return
     if instance.accepted == -1 and another_request == 1:
         # Сказать второму, что его запрос отклонили
         async_to_sync(channel_layer.group_send)(f'recipient_{instance.second_user.id}',
-                                                {"type": "friend_message", "data": {'type': 'friend_denied',
+                                                {"type": "send_friend", "data": {'type': 'friend_denied',
                                                                                   'description': "Запрос в друзья отклонён",
                                                                                   'user': serializer.data}})
         return
@@ -46,14 +54,14 @@ def create_user_channel(sender, instance: Friends, **kwargs):
     if not another_request and instance.accepted == 0:
         # Сказать второму, что пользователь передумал отправлять запрос в друзья
         async_to_sync(channel_layer.group_send)(f'recipient_{instance.second_user.id}',
-                                                {"type": "friend_message", "data": {'type': 'friend_canceled',
+                                                {"type": "send_friend", "data": {'type': 'friend_canceled',
                                                                                   'description': "Запрос в друзья отменён",
                                                                                   'user': serializer.data}})
         return
     if another_request[0].accepted == 1 and instance.accepted == 1:
         # Сказать второму, что его удалили из друзей
         async_to_sync(channel_layer.group_send)(f'recipient_{instance.second_user.id}',
-                                                {"type": "friend_message", "data": {'type': 'friend_delete',
+                                                {"type": "send_friend", "data": {'type': 'friend_delete',
                                                                                   'description': "Удалён из друзей",
                                                                                   'user': serializer.data}})
         return
